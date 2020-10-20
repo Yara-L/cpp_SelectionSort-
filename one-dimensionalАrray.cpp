5b3dbcb7864cb63b5fc0bd54a0a45aece07b8479
Yaroslava L <66276044+Yara-L@users.noreply.github.com>
2020-10-20 23:23:52 +0300

    One-Dimensional Array
    One-Dimensional Array is sorted by Selection Sort

---
+++ one-dimensionalАrray.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	const int n = 9;
+//	unsigned int arr[n]{9, 3, 4, 2, 1, 0, 7, 5, 6};
+//	int num;
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = i + 1; j < n; j++ ) {
+//			if (arr[i] > arr[j]) {
+//				num = arr[i];
+//				arr[i] = num;
+//				if (arr[j] > num) {
+//					num = arr[j];
+//					arr[j] = arr[i];
+//					arr[i] = num;
+//				}
+//			}
+//			else if (arr[i] < arr[j]) {
+//				num = arr[j];
+//				arr[j] = arr[i];
+//				arr[i] = num;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		cout << arr[i] << endl;
+//	}
+//
+//	return 0;
+//}
